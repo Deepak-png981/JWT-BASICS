89c1ad1cc5e7ce13d9e2f071315f334b7baa1aea
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-11-19 13:13:09 +0530

    Just had a look at jwt and its working by taking some examples and using postman by sending authentication bearer!! implemented it in my fileauth project

---
+++ controllers/main.js
@@ -0,0 +1,23 @@
+const CustomAPIError = require('../errors/custom-error');
+const jwt = require('jsonwebtoken');
+const login = async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        throw new CustomAPIError('Please provide email and password', 400);
+    }
+
+    //dummy id for now
+    const id = new Date().getDate();
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    });
+    res.status(200).json({ msg: 'User logged in successfully', token });
+}
+
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({ msg: `Hello ${req.user.username}`, secret: `here is your secret : ${luckyNumber}` });
+}
+module.exports = {
+    login, dashboard
+}
